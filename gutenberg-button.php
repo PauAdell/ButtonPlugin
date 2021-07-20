8f71811745aa82f09cae604bc4ba53eaf9aae8d0
PauAdell <pau.adell1@gmail.com>
2021-07-20 10:34:22 +0200

    FirstCommit

---
+++ gutenberg-button.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * Plugin Name: Gutenberg Button
+ * Plugin URI:  https://github.com/PauAdell/ButtonPlugin
+ * Description: Additional Gutenberg button.
+ * Version:     0.1
+ * 
+ * Author:      Pau Adell
+ * 
+ */
