1ca1b5863dd9b499cd3ee3f4c213f4cb7111b216
PauAdell <pau.adell1@gmail.com>
2021-07-20 10:45:36 +0200

    Start

--- gutenberg-button.php
+++ gutenberg-button.php
@@ -9,3 +9,14 @@
  * Author:      Pau Adell
  * 
  */
+
+ namespace Guttenberg_Button;
+
+ defined( 'ABSPATH' ) or die();
+
+ function init_constants() {
+    define( 'GUTENBERG_BUTTON_URL', untrailingslashit( plugin_dir_url(__FILE__) ) );
+    define( 'GUTENBERG_BUTTON_PATH', untrailingslashit( plugin_dir_path(__FILE__) ) );
+    /* define( 'GUTENBERG_BUTTON_VERSION', get_file_data( __FILE__, [ 'Version' ], 'plugin')[0] ) ); */
+ }
+ add_action( 'plugins_loaded', __NAMESPACE__ . '\init_constants', 1);
